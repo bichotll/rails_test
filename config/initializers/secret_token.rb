bfca40a36f9b40b02974c3d578320345ab37455e
bichotll <jaume@bic.cat>
2013-03-10 17:04:00 +0000

    hi world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homestay::Application.config.secret_token = '480eeb6da22e77d3068f8785d5c0becd8dffa617d428fab7ad835c50a7b17d45bc29b4b4e4f2e9450d55fbb1af3a57dd2306febd176a74ee0eff6d1775780261'
